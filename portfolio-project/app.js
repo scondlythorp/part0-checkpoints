3278dc1efba2e783f23345dc02f7ca3359a5eae0
scondlythorp <modoulaminthorp4@gmail.com>
2026-03-15 23:30:11 +0000

    Add console output for current fares and statistics

--- portfolio-project/app.js
+++ portfolio-project/app.js
@@ -1,1 +1,25 @@
 const { fares } = require("./data.js");
+const { createFare, findFareById, updateFare, deleteFare } = require("./crud.js");
+const { calculateAverageFare, getMostExpensiveRoute, getCheapestVehicleType, getMostCommonRoute } = require("./enhancements.js");
+const { faresByVehicle, faresByRoute } = require("./data-transformation.js");
+
+console.log("===== CURRENT FARES =====");
+console.table(fares);
+
+console.log("\n===== ADD A NEW FARE =====");
+console.log(createFare({ from: "Bakau", to: "Brikama", vehicleType: "Taxi", price: 22 }));
+
+console.log("\n===== UPDATED FARES =====");
+console.table(fares);
+
+console.log("\n===== STATISTICS =====");
+console.log("Average fare:", calculateAverageFare());
+console.log("Most expensive route:", getMostExpensiveRoute());
+console.log("Cheapest vehicle type:", getCheapestVehicleType());
+console.log("Most common route:", getMostCommonRoute());
+
+console.log("\n===== FARES GROUPED BY VEHICLE =====");
+console.log(faresByVehicle);
+
+console.log("\n===== FARES GROUPED BY ROUTE =====");
+console.log(faresByRoute);
