a5349ab59708e7d1ecf57bbb9244d9645111e7c8
scondlythorp <modoulaminthorp4@gmail.com>
2026-03-15 21:31:46 +0000

    Add initial student data (10 students)

---
+++ student-tracker/data.js
@@ -0,0 +1,78 @@
+const students = [
+  {
+    id: 1,
+    name: "Modou",
+    age: 21,
+    grade: "A",
+    attendance: 90
+  },
+  {
+    id: 2,
+    name: "Fatou",
+    age: 20,
+    grade: "B",
+    attendance: 85
+  },
+  {
+    id: 3,
+    name: "Lamin",
+    age: 22,
+    grade: "C",
+    attendance: 75
+  },
+  {
+    id: 4,
+    name: "Awa",
+    age: 19,
+    grade: "A",
+    attendance: 95
+  },
+  {
+    id: 5,
+    name: "Ousman",
+    age: 23,
+    grade: "B",
+    attendance: 80
+  },
+  {
+    id: 6,
+    name: "Mariama",
+    age: 21,
+    grade: "A",
+    attendance: 88
+  },
+  {
+    id: 7,
+    name: "Alieu",
+    age: 22,
+    grade: "C",
+    attendance: 70
+  },
+  {
+    id: 8,
+    name: "Isatou",
+    age: 20,
+    grade: "B",
+    attendance: 82
+  },
+  {
+    id: 9,
+    name: "Bakary",
+    age: 24,
+    grade: "A",
+    attendance: 91
+  },
+  {
+    id: 10,
+    name: "Haddy",
+    age: 21,
+    grade: "B",
+    attendance: 86
+  }
+];
+
+// Export for use in other files
+
+module.exports = {
+    students
+};
