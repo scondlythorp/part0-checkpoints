5ca962c69e509b990794cc14adabb8f396a9f42e
scondlythorp <modoulaminthorp4@gmail.com>
2026-03-15 20:29:43 +0000

    Add array of Gambian cities

---
+++ js-fundamentals-quiz.js
@@ -0,0 +1,11 @@
+// 1. Array of Gambian Regions
+const regions = [
+  "Lower River Region",
+  "West Coast Region",
+  "North Bank Region",
+  "Central River Region",
+  "Upper River Region"
+];
+
+console.log("Regions:", regions);
+
