bb4f634f03e870f1a5ca6e020912dd055a800a17
scondlythorp <modoulaminthorp4@gmail.com>
2026-03-15 20:42:51 +0000

    Add array of fare objects

--- js-fundamentals-quiz.js
+++ js-fundamentals-quiz.js
@@ -20,3 +20,13 @@ const taxiFare = {
 };
 
 console.log("Taxi Fare:", taxiFare); 
+
+// 3. Array of Fare Objects
+const fares = [
+  { id: 1, from: "Tabokoto",  to: "Latrikunda", vehicle: "Taxi",     price: 15 },
+
+  { id: 2, from: "WestField", to: "Gimpex",     vehicle: "Bus",      price: 12 },
+  
+  { id: 3, from: "Bakau",     to: "Serekunda",  vehicle: "Gelegele", price: 8  }
+];
+
