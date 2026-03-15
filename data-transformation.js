57f51418d46bdc7ebd952f437090199829fd5264
scondlythorp <modoulaminthorp4@gmail.com>
2026-03-15 23:37:42 +0000

    Add arrays of fare objects

---
+++ data-transformation.js
@@ -0,0 +1,17 @@
+/*
+=====================================================
+PASSO Data Transformation Practice
+Checkpoint 0d.4
+=====================================================
+*/
+
+const fares = [
+  { id: 1, from: "Banjul", to: "Serekunda", vehicleType: "Taxi", price: 25 },
+  { id: 2, from: "Banjul", to: "Serekunda", vehicleType: "Bus", price: 12 },
+  { id: 3, from: "Brikama", to: "Banjul", vehicleType: "Taxi", price: 30 },
+  { id: 4, from: "Brikama", to: "Banjul", vehicleType: "Bus", price: 18 },
+  { id: 5, from: "Bakau", to: "Serekunda", vehicleType: "Gelegele", price: 10 },
+  { id: 6, from: "Bakau", to: "Serekunda", vehicleType: "Taxi", price: 20 },
+  { id: 7, from: "Farafenni", to: "Banjul", vehicleType: "7-Seater", price: 50 },
+  { id: 8, from: "Farafenni", to: "Banjul", vehicleType: "Bus", price: 35 }
+];
