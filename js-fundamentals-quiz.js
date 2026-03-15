723d2e04bf8c7517e94ff56aa91d0a39eb70ba9c
scondlythorp <modoulaminthorp4@gmail.com>
2026-03-15 20:34:38 +0000

    Add taxi fare object

--- js-fundamentals-quiz.js
+++ js-fundamentals-quiz.js
@@ -1,11 +1,10 @@
-// 1. Array of Gambian Regions
-const regions = [
-  "Lower River Region",
-  "West Coast Region",
-  "North Bank Region",
-  "Central River Region",
-  "Upper River Region"
-];
+// 2. Taxi Fare Object
+const taxiFare = {
+  id: 1,
+  from: "Banjul",
+  to: "Serekunda",
+  vehicleType: "Taxi",
+  price: 15
+};
 
-console.log("Regions:", regions);
-
+console.log("Taxi Fare:", taxiFare); 
